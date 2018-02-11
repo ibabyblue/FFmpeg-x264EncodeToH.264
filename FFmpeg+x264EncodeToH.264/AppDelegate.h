bb55eac762e9a1792c34d0bbe6000fb35043c5a2
zhangbiao <zhangbiao@dongdao.net>
2018-02-11 09:46:46 +0800

    Initial Commit

---
+++ FFmpeg+x264EncodeToH.264/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFmpeg+x264EncodeToH.264
+//
+//  Created by ibabyblue on 2018/2/11.
+//  Copyright © 2018年 ibabyblue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
